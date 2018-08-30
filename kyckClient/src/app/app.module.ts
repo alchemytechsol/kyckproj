f407b7db7eb3a6d57e0ecec6068f66e8512b682f
Ravindra447 <ravindra.ch447@gmail.com>
2018-08-30 15:28:01 +0530

    initial components created

--- kyckClient/src/app/app.module.ts
+++ kyckClient/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './component/home/home.component';
+import { NavComponent } from './component/nav/nav.component';
+import { LoginComponent } from './component/login/login.component';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavComponent,
+    LoginComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
